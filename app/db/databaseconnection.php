13d292bdf2df4b4e4465c56028882243452d53be
jadwana <jadwana@yahoo.fr>
2022-11-18 13:37:02 +0100

    creation of the post model #5

---
+++ app/db/databaseconnection.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\db;
+
+class DatabaseConnection
+{
+    public ?\PDO $database = null;
+    
+
+    public function getConnection(): \PDO
+    {
+        if($this->database === null){
+            $this->database = new \PDO('mysql:host=;dbname=blog;charset=utf8', 'root', '');
+        }
+        return $this->database;
+    }
+
+}
