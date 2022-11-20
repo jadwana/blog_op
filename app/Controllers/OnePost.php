a8ed5565242c38adf2f225b9ec348eae71ac0a00
jadwana <jadwana@yahoo.fr>
2022-11-20 10:09:43 +0100

    creation of the comment model #11

--- app/Controllers/OnePost.php
+++ app/Controllers/OnePost.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 
 use App\Models\Post;
+use App\Models\Comment;
 use App\db\DatabaseConnection;
 require 'vendor/autoload.php';
 
@@ -16,12 +17,12 @@ class OnePost extends Controller
         $connection = new DatabaseConnection();
 
         $postRepository = new Post();
-        
+        $commentRepository = new Comment();
         $postRepository->connection = $connection;
-    
+        $commentRepository->connection = $connection;
         $post = $postRepository->getPost($identifier);
-        
+        $comments = $commentRepository->getComments($identifier);
 
-        $this->twig->display('onepost.twig', ['post'=> $post]);
+        $this->twig->display('onepost.twig', ['post'=> $post, 'comments'=>$comments]);
     }
 }
