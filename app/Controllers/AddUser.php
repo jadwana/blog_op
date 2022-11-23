d212aa30c2bdc32ff637dea03b6353b8a553320b
jadwana <jadwana@yahoo.fr>
2022-11-23 16:27:50 +0100

    adding regestration of a new user #22

---
+++ app/Controllers/AddUser.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\user;
+use App\db\DatabaseConnection;
+require 'vendor/autoload.php';
+
+
+class AddUser extends Controller
+{
+    //on fait les vérifications et on securise les entrées
+    public function execute()
+    {
+        if(!empty($_POST)){
+            if(isset($_POST["username"], $_POST["password"]) && !empty($_POST["username"]) && !empty($_POST["password"])){
+
+                $username= strip_tags(trim($_POST['username']));
+                if(strlen($username)<5){
+                    throw new \Exception('votre pseudo est trop court!');
+                }
+                    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                        echo 'adresse mail incorrecte';
+                    }
+                    // $pseudo=htmlspecialchars($_POST['pseudo']);
+                    $email=$_POST['email'];
+
+                    //we check that the nickname is unique
+                    $usernameCheck = new User();
+                    $usernameCheck->connection= new DatabaseConnection();
+                    $result1 = $usernameCheck->checkUserUsername($username);
+                        if($result1){
+                            throw new \Exception('pseudo déjà existant !');
+                        }
+
+                    //we check that the email is unique
+                    $userMailCheck = new User();
+                    $userMailCheck->connection= new DatabaseConnection();
+                    $result2 = $userMailCheck->checkUserEmail($email);
+                        if($result2){
+                            throw new \Exception('email déjà existant !');
+                        }
+
+                    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+                    //we add the new user
+                    $userRepository = new User();
+                    $userRepository->connection = new DatabaseConnection();
+                    $success = $userRepository->addUser($username, $pass, $email);
+                        if(!$success){
+                            throw new \Exception('Impossible d\'ajouter l\'utilisateur !');
+                        }
+                        $usersession = new User();
+                        $usersession->connection= new DatabaseConnection();
+                        $sessionResult = $usersession->checkUserUsername($username);
+                        $_SESSION['user_id']= $sessionResult->getUser_id;
+                        $_SESSION['username']= $sessionResult->getUsername;
+                        $_SESSION['role'] = $sessionResult->getRole;
+                        header('location: index.php');
+            }
+            else{
+                echo 'toutes les informations doivent être complétées';
+            }
+        }
+
+        $this->twig->display('register.twig');
+        
+    }
+}
